46abd83f56b396c818a9fb906971863ff7c6b68f
Anthony Scatchell <scatchell@gmail.com>
2018-08-11 19:30:24 +0100

    Adding wearable compass file

---
+++ src/WearableCompass.ino
@@ -0,0 +1,111 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+#include "Display.h"
+#include "Direction.h"
+#include <math.h>
+
+/* Set the delay between fresh samples */
+#define BNO055_SAMPLERATE_DELAY_MS (100)
+
+/*
+   Connections
+   ===========
+   BNO055
+   Connect SCL to analog 5
+   Connect SDA to analog 4
+   Connect VDD to 3.3V DC
+   Connect GROUND to common ground
+*/
+
+Adafruit_BNO055 bno = Adafruit_BNO055();
+Display ledMatrix = Display();
+Direction direction = Direction();
+imu::Vector<3> vector;
+
+
+void displaySensorDetails(void)
+{
+  sensor_t sensor;
+  bno.getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" xxx");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" xxx");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" xxx");
+  Serial.println("------------------------------------");
+  Serial.println("");
+  delay(500);
+}
+
+void displayTemperature(void)
+{
+
+  /* Display the current temperature */
+  int8_t temp = bno.getTemp();
+  Serial.print("Current Temperature: ");
+  Serial.print(temp);
+  Serial.println(" C");
+  Serial.println("");
+}
+
+void displayCalStatus(void)
+{
+  /* Get the four calibration values (0..3) */
+  /* Any sensor data reporting 0 should be ignored, */
+  /* 3 means 'fully calibrated" */
+  uint8_t system, gyro, accel, mag;
+  system = gyro = accel = mag = 0;
+  bno.getCalibration(&system, &gyro, &accel, &mag);
+
+  /* The data should be ignored until the system calibration is > 0 */
+  Serial.print("\t");
+  if (!system)
+  {
+    Serial.print("! ");
+  }
+
+  /* Display the individual values */
+  Serial.print("Sys:");
+  Serial.print(system, DEC);
+  Serial.print(" G:");
+  Serial.print(gyro, DEC);
+  Serial.print(" A:");
+  Serial.print(accel , DEC);
+  Serial.print(" M:");
+  Serial.println(mag, DEC);
+}
+
+void setup(){
+  Serial.begin(9600);
+  Serial.println("Wearable Compass"); Serial.println("");
+
+  /* Initialise the sensor */
+  if(!bno.begin(Adafruit_BNO055::OPERATION_MODE_COMPASS))
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+
+  delay(1000);
+
+  displaySensorDetails();
+  displayTemperature();
+  displayCalStatus();
+
+  bno.setExtCrystalUse(true);
+}
+
+
+void loop(){
+  sensors_event_t event;                   // Read 9DOF Sensor
+  bno.getEvent(&event);
+  Serial.print("Direction integer: ");
+  Serial.println(direction.directionToInteger(event.orientation.x), DEC);
+  // displayCalStatus();
+  delay(BNO055_SAMPLERATE_DELAY_MS);
+}
