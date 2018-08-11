70f2ecb16264a458c9b4337fb2131382af84fe6d
= <=>
2018-08-11 12:40:57 +0100

    Reading the temperature from the sensor

--- WearableCompass.ino
+++ WearableCompass.ino
@@ -1,11 +1,42 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+/* 
+   Connections
+   ===========
+   BNO055
+   Connect SCL to analog 5
+   Connect SDA to analog 4
+   Connect VDD to 3.3V DC
+   Connect GROUND to common ground
+*/
+
+Adafruit_BNO055 bno = Adafruit_BNO055();
+
 void setup(){
+
   Serial.begin(9600);
-  Serial.println("Hello World");
-  pinMode(13, OUTPUT);
+  Serial.println("Orientation Sensor Raw Data Test"); Serial.println("");
+
+  /* Initialise the sensor */
+  if(!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+
+  delay(1000);
+
+  /* Display the current temperature */
+  int8_t temp = bno.getTemp();
+  Serial.print("Current Temperature: ");
+  Serial.print(temp);
+  Serial.println(" C");
+  Serial.println("");
+
 }
 void loop(){
-  digitalWrite(13, HIGH);
-  delay(1000);
-  digitalWrite(13, LOW);
-  delay(1000);
 }
